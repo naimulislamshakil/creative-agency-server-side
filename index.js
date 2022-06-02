724ef96b8d8c673bb39a44d39d9e14f4d5530494
naimulislamshakil <ms0413354@gmail.com>
2022-06-02 06:58:11 +0600

    index.js file setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const verify = require("jsonwebtoken/verify");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// madelware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("How are you?");
+});
+
+app.listen(port, () => {
+  console.log(`creative agency server side runing ${port}`);
+});
