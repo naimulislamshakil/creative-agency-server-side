d22db6be5876190a0f4e8f5f3f3940d51f1fdb28
naimulislamshakil <ms0413354@gmail.com>
2022-06-02 07:20:50 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// AqRLK3lUECwqegsM;
+// creative - agency;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xwyd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log("db connect");
+const run = async () => {
+  try {
+    await client.connect();
+  } finally {
+    // await client.close()
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("How are you?");
 });
