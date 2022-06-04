24998ebd7200d0a337198ff8d8e535ff4d44eca0
naimulislamshakil <ms0413354@gmail.com>
2022-06-04 06:13:18 +0600

    give all available services

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const verify = require("jsonwebtoken/verify");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// const verify = require("jsonwebtoken/verify");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// ObjectId,
+// Collection,
 // AqRLK3lUECwqegsM;
 // creative - agency;
 
@@ -24,10 +26,23 @@ console.log("db connect");
 const run = async () => {
   try {
     await client.connect();
+
+    //   awesome Collection
+    const asesomeServiceCollection = client
+      .db("creative_agency")
+      .collection("asesome_service");
+
+    // get all asesomeServiceCollection
+    app.get("/service", async (req, res) => {
+      const result = await asesomeServiceCollection.find().toArray();
+      res.send(result);
+    });
   } finally {
     // await client.close()
   }
 };
+
+run(), console.dir();
 
 app.get("/", (req, res) => {
   res.send("How are you?");
